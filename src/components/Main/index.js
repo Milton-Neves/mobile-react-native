742a6872db0a47103f0b6bdcf6b2c422a1bd785c
Milton <milzonzneves@gmail.com>
2022-09-05 09:39:58 -0300

    implementando formulario

---
+++ src/components/Main/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View } from "react-native";
+import Form from "../Form/";
+//AQUI, COLOQUEI TODO O CONTEÚDO
+export default function Main() {
+    return (
+        <View>
+            <Form/>
+        </View>
+    )
+}
